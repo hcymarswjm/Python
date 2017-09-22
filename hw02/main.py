763811a08857f97f2fb30852af412162ea4c7a0e
hcymarswjm <1812983972@qq.com>
2017-09-22 16:36:56 +0800

    Create main.py

---
+++ hw02/main.py
@@ -0,0 +1,19 @@
+def bsearch(data, target):
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = int((low + high) / 2)
+        if target==data[mid]:
+            return mid
+        elif target < data[mid]:
+            high = mid - 1
+        elif target > data[mid]:
+            low = mid + 1
+        else:
+            return mid
+    return None
+ 
+if __name__ == '__main__':
+    array = [1,2,3,4,5]
+    result = bsearch(array,5)
+    print(result)
